ab62827511bac091ce1d0babac9ec5d5817c30bc
junghwa1996 <59001439+junghwa1996@users.noreply.github.com>
2020-08-11 20:58:23 +0900

    Add files via upload

---
+++ Moblie/js/fixd.js
@@ -0,0 +1,60 @@
+$(function(){
+    tapMenu();
+    Menuopen();
+    MSearchopen();
+    scrollTopbtn();
+    quickFixed();
+    navFixed();
+});
+
+
+// 모바일 시작
+
+function tapMenu(){
+    $('.tapwrap>ul>li').click(function(){
+        $('.tapwrap>ul>li').removeClass('tabOn');
+        $(this).addClass('tabOn');
+    });
+}
+// 좌측 메뉴 탭기능
+
+function navFixed(){
+    var nav = $('.middle').height();
+    $(window).scroll(function(){
+        if($(this).scrollTop() > nav){
+            $('header').addClass('fixedNav');
+        }else{
+            $('header').removeClass('fixedNav');
+        }
+    });
+}
+
+
+function Menuopen(){
+    $('.asideMenuIco').click(function(){
+        $('.sidebar').toggleClass('open');
+    });
+}
+// 좌측 메뉴 오픈
+function MSearchopen(){
+    $('.search_Btn').click(function(){
+        $('.search form').toggleClass('searchOn');
+    });
+}
+
+function scrollTopbtn(){
+    $('.Topico').on('click',function(){
+        $('html,body').stop().animate({scrollTop:0},1000);
+    });
+}
+
+function quickFixed(){
+    var nav = $('.middle').height();
+    $(window).scroll(function(){
+        if($(this).scrollTop() > nav){
+            $('.Topbtn').addClass('block');
+        }else{
+            $('.Topbtn').removeClass('block');
+        }
+    });
+}
---
+++ Moblie/js/main_ctn.js
@@ -0,0 +1,66 @@
+$(function(){
+    mainslide();
+    minislide();
+    recommend_swiper();
+    textbanner();
+});
+
+
+function mainslide(){
+    var swiper = new Swiper('.swiper_main', {
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+          },
+          loop: true,
+      });
+}
+
+function minislide(){
+  var swiper = new Swiper('.mini_swiper', {
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    loop: true,
+  });
+}
+
+function recommend_swiper(){
+  var swiper = new Swiper('.recommend_swiper', {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    slidesPerGroup: 2,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    loop: true,
+  });
+}
+
+function textbanner(){
+  var swiper = new Swiper('.text_swiper', {
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    loop: true,
+  });
+}
